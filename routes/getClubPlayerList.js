31830bc18c23f64c2ad98ea92ffebb8b5c83c7a1
Bhuvnesh Maheshwari <bhuvnesh13396@gmail.com>
2019-03-25 23:44:06 +0530

    Query from mutilple tables in /getClubPlayerList route

--- routes/getClubPlayerList.js
+++ routes/getClubPlayerList.js
@@ -6,7 +6,8 @@ module.exports = (app) => {
         
         const clubName = req.params.clubName;
 
-        // Fetch playerId from Affiliation table
+        // Fetch all the playerIds playing for same club
+        // from Affiliation table
         PlayerAffiliations.findAll({
 
             attributes: ['ID'],
@@ -14,8 +15,60 @@ module.exports = (app) => {
                 Club : clubName
             }
         })
-            .then(playerId => {
-                console.log(`Player ID found ${playerId}`);
+            .then(playerIds => {
+                console.log(`Player IDs found ${playerIds}`);
+
+                Player.findAll({
+                    include : [{
+                        model : [PlayerAffiliations, PlayerStats, PlayerTraits] ,
+                        required : true,
+                        where : { ID : playerIds }
+                    }]
+                })
+                    .then(allClubPlayerList => {
+                        console.log(`List of all club players ${allClubPlayerList}`);
+                        res.status(200).json(allClubPlayerList);
+                    });
+
+                // // Query player's affilations table
+                // let playerAffiliations = PlayerAffiliations.findAll({
+                //     where : {
+                //         ID : playerIds
+                //     }
+                // });
+
+                // // Query Player's personal detail table
+                // let personalDetails = Player.findAll({
+                //     where : {
+                //         ID : playerIds
+                //     }
+                // });
+                    
+                // // Query player's stats table
+                // let playerStats = PlayerStats.findAll({
+                //     where : {
+                //         ID : playerIds
+                //     }
+                // });
+                       
+
+                // // Query player's traits table
+                // let playerTraits = PlayerTraits.findAll({
+                //     where : {
+                //         ID : playerIds
+                //     }
+                // });
+                    
+                // // Return response after quering all the 
+                // // tables(affiliation, stats, trait)
+                // Promise.all([playerAffiliations, playerStats, playerTraits]).then(values => {
+                //     playerDetails.affiliation = values[0];
+                //     playerDetails.stats = values[1];
+                //     playerDetails.traits = values[2];
+
+                //     res.status(200).json(playerDetails);
+                // });
+
             })
 
             .catch(err => {
